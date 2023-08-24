1af1e6cd8a5cbd16a6f32b95f60119802168697a
Gnlow <gnlow@protonmail.com>
2023-08-25 01:37:16 +0900

    korean

--- script/README.md
+++ script/README.md
@@ -3,3 +3,6 @@
 ## 출처
 
 - `german.tsv`: https://www.sttmedia.com/characterfrequency-german
+- `english.tsv`: https://www.sttmedia.com/characterfrequency-english
+- `korean.tsv`: https://yottu.tistory.com/13
+    - 0인 값 삭제
--- script/mod.ts
+++ script/mod.ts
@@ -10,10 +10,12 @@ const getEntropy =
     .map(n => n * Math.log2(1/n))
     .reduce((a, b) => a + b, 0)
 
-const file = await Deno.readTextFile("data/english.tsv")
+const file = await Deno.readTextFile("data/korean.tsv")
 const words = parseTsv(file)
 const counts = words.map(([_, count]) => count)
+const total = counts.reduce((a, b) => a + b, 0)
 const entropy = getEntropy(counts)
+console.log(total)
 console.log(entropy)
 
     
