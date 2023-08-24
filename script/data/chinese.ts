ac59db35c5dc19de93f1933b63e811c6579500b2
Gnlow <gnlow@protonmail.com>
2023-08-25 02:10:37 +0900

    fix chinese

--- script/data/chinese.ts
+++ script/data/chinese.ts
@@ -6,7 +6,7 @@ const parseTsv =
     (tsv: string) =>
     tsv.split("\n")
     .map(line => line.split("\t"))
-    .map(([_i, char, _n, cumFreq]) => [char, Number(cumFreq)] as [string, number])
+    .map(([_i, char, _n, cumFreq]) => [char, Number(cumFreq) / 100] as [string, number])
     .map(([char, cumFreq], i, l) => [char, cumFreq - (l[i-1]?.[1] || 0)])
 
 const result = parseTsv(file)
